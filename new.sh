e2d29d316a19e3cc6bf54378573f8663b097c9fa
SarthakGodge <sarthakgodge@gmail.com>
2025-04-08 14:56:33 +0530

    hex created

---
+++ new.sh
@@ -0,0 +1,17 @@
+echo "Enter two hexadecimal numbers:"
+read h1 h2
+echo "Choose operation: + - * /"
+read op
+
+# Convert hex to decimal
+a=$((16#$h1))
+b=$((16#$h2))
+
+case $op in
+  +) res=$(($a + $b)) ;;
+  -) res=$(($a - $b)) ;;
+  \*) res=$(($a * $b)) ;;
+  /) res=$(($a / $b)) ;;
+  *) echo "Invalid operation"; exit 1 ;;
+
+esac
